8d6f4e75474ec2950f3d8bd5d48b3ee5b2d58908
Jay <jaymangukiya@gmail.com>
2022-12-24 18:53:37 +0530

    First Commit

---
+++ resources/views/content/dashboard/dashboards-analytics.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts/contentNavbarLayout')
+
+@section('title', 'Dashboard - Analytics')
+
+@section('vendor-style')
+<link rel="stylesheet" href="{{asset('assets/vendor/libs/apex-charts/apex-charts.css')}}">
+@endsection
+
+@section('vendor-script')
+<script src="{{asset('assets/vendor/libs/apex-charts/apexcharts.js')}}"></script>
+@endsection
+
+@section('page-script')
+<script src="{{asset('assets/js/dashboards-analytics.js')}}"></script>
+@endsection
+
+@section('content')
+
+<h1>Dashboard</h1>
+
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto corrupti, provident consectetur harum autem unde?</p>
+@endsection
