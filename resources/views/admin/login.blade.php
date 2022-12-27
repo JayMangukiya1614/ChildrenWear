86712cae738ff17e209fd8b1ab2d31648e9c3a2b
Jay <jaymangukiya@gmail.com>
2022-12-27 14:38:03 +0530

    Admin login

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -9,198 +9,76 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
 
     <title>Admin-Log-In</title>
+	<link rel="stylesheet" href="AdminloginCss/css/style.css">
+
 </head>
-<style>
-    * {
-        box-sizing: border-box;
-    }
 
-    body {
-        background-color: #EEE;
-    }
-
-    .clear {
-        clear: both;
-    }
-
-    .log {
-        width: 400px;
-        margin: 5% auto;
-        background-color: #FFF;
-        padding: 30px 0;
-    }
-
-    .log h2 {
-        text-align: center;
-        color: #ed2553;
-        font-weight: bold;
-        font-size: 26px;
-        margin-bottom: 50px;
-    }
-
-    .log .input-cont {
-        position: relative;
-        margin: 0 50px 60px;
-    }
-
-    .log .input-cont:last-of-type {
-        margin-bottom: 30px;
-    }
-
-    .log .input-cont input {
-        position: relative;
-        z-index: 1;
-        width: 100%;
-        height: 40px;
-        outline: none;
-        color: #212121;
-        font-size: 22px;
-        font-weight: 400;
-        background: none;
-        border: none;
-    }
-
-    .log .input-cont input:focus {
-        outline: none;
-    }
-
-    .log .input-cont label {
-        position: absolute;
-        color: #948c8c;
-        top: 0;
-        left: 0;
-        line-height: 40px;
-        -webkit-transition: .3s;
-        -moz-transition: .3s;
-        -o-transition: .3s;
-        transition: .3s;
-    }
-
-    .log .input-cont input:focus+label {
-        margin-top: -30px;
-        -webkit-transform: scale(.8);
-        -moz-transform: scale(.8);
-        -o-transform: scale(.8);
-        transform: scale(.8);
-        color: #bdbdbd;
-    }
-
-    .log .border1,
-    .log .border2 {
-        position: absolute;
-        height: 1px;
-        background-color: #9E9E9E;
-        left: 0;
-        bottom: 0;
-        width: 100%;
-    }
-
-    .log .border1::after,
-    .log .border1::before,
-    .log .border2::after,
-    .log .border2::before {
-        content: "";
-        position: absolute;
-        bottom: 0;
-        width: 0;
-        height: 2px;
-        -webkit-transition: .5s;
-        -moz-transition: .5s;
-        -o-transition: .5s;
-        transition: .5s;
-    }
-
-    .log .border1::after,
-    .log .border2::after {
-
-        right: 50%;
-        background-color: #ed2553;
-    }
-
-    .log .border1::before,
-    .log .border2::before {
-        left: 50%;
-        background-color: #ed2553;
-    }
-
-    .log .input-cont input:focus~.border1::after,
-    .log .input-cont input:focus~.border1::before,
-    .log .input-cont input:focus~.border2::after,
-    .log .input-cont input:focus~.border2::before {
-        width: 50%;
-    }
-
-    .log .check,
-    .log a {
-        float: left;
-        width: calc(50% - 50px);
-        display: block;
-        font-size: 12px;
-        margin-bottom: 30px;
-    }
-
-    .log .check {
-        margin-left: 50px;
-    }
-
-    .log a {
-        text-align: right;
-        text-decoration: none;
-        color: #ed2553;
-    }
-
-    .log a:hover {
-        text-decoration: underline;
-        color: #F00;
-    }
-
-    .log form input[type="submit"] {
-        display: block;
-        margin: 0 auto 20px;
-        border: 2px solid transparent;
-        padding: 5px 20px;
-        font-size: 22px;
-        cursor: pointer;
-        color: #ed2553;
-        -webkit-transition: .5s;
-        -moz-transition: .5s;
-        -o-transition: .5s;
-        transition: .5s;
-    }
-
-    .log form input[type="submit"]:focus {
-        outline: none;
-    }
-
-    .log form input[type="submit"]:hover {
-        border: 2px solid #ed2553;
-    }
-</style>
 
 <body>
-    <div class="log">
-        <h2>Welcome To Baby Hub</h2>
-        <form>
-            <div class="input-cont">
-                <input type="text">
-                <label>Username</label>
-                <div class="border1"></div>
+    <section class="ftco-section">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 text-center mb-5">
+                    {{-- <h2 class="heading-section">Login #03</h2> --}}
+                </div>
             </div>
-            <div class="input-cont">
-                <input type="password">
-                <label>Password</label>
-                <div class="border2"></div>
+            <div class="row justify-content-center">
+                <div class="col-md-7 col-lg-5">
+                    <div class="login-wrap p-4 p-md-5">
+                        <div class="d-flex">
+                            <div class="w-100">
+                                <h3 class="mb-4">Sign In</h3>
+                            </div>
+                            <div class="w-100">
+                                <p class="social-media d-flex justify-content-end">
+                                    <a href="#"
+                                        class="social-icon d-flex align-items-center justify-content-center"><span
+                                            class="fa fa-facebook"></span></a>
+                                    <a href="#"
+                                        class="social-icon d-flex align-items-center justify-content-center"><span
+                                            class="fa fa-twitter"></span></a>
+                                </p>
+                            </div>
+                        </div>
+                        <form action="#" class="login-form">
+                            <div class="form-group">
+                                <div class="icon d-flex align-items-center justify-content-center"><span
+                                        class="fa fa-user"></span></div>
+                                <input type="text" class="form-control rounded-left" placeholder="Username" required>
+                            </div>
+                            <div class="form-group">
+                                <div class="icon d-flex align-items-center justify-content-center"><span
+                                        class="fa fa-lock"></span></div>
+                                <input type="password" class="form-control rounded-left" placeholder="Password"
+                                    required>
+                            </div>
+                            <div class="form-group d-flex align-items-center">
+                                <div class="w-100">
+                                    <label class="checkbox-wrap checkbox-primary mb-0">Save Password
+                                        <input type="checkbox" checked>
+                                        <span class="checkmark"></span>
+                                    </label>
+                                </div>
+                                <div class="w-100 d-flex justify-content-end">
+                                    <button type="submit" class="btn btn-primary rounded submit">Login</button>
+                                </div>
+                            </div>
+                            <div class="form-group mt-4">
+                                <div class="w-100 text-center">
+                                    <p class="mb-1">Don't have an account? <a href="#">Sign Up</a></p>
+                                    <p><a href="#">Forgot Password</a></p>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <span class="check">
-                <input type="checkbox"> <label>Remember Me</label>
-            </span>
-            <a href="#">Forgot Password</a>
-            <div class="clear"></div>
-            <input type="submit" value="Sign In">
-        </form>
-    </div>
+        </div>
+    </section>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -212,6 +90,14 @@
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script>
+        (function($) {
+
+            "use strict";
+
+
+        })(jQuery);
     </script>
 </body>
 
