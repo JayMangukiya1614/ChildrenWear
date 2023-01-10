e0891e4f29a039847c26ef0df9751029dcb935cf
krupali <krupali@gmail.com>
2023-01-10 20:57:30 +0530

    WIP in Auth

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Http\Requests\AdminRequest;
 use App\Models\Adminreg;
 
@@ -8,49 +9,32 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function AdminLogin()
-    {
-        return view('admin.login')->with('LogOut','LogOut Successfully....!');
-    }
+  public function AdminLogin()
+  {
+    return view('admin.login')->with('LogOut', 'LogOut Successfully....!');
+  }
 
-    public function AdminProfile()
-    {
-        return view('admin.Profile');
-    }
-    public function AdminReg()
-    {
-        return view('admin.Registeration');
-    }
+  public function AdminProfile()
+  {
+    return view('admin.Profile');
+  }
+  public function AdminReg()
+  {
+    return view('admin.Registeration');
+  }
 
-    public function AdminRegSave(Request $req)
-    {
-<<<<<<< Updated upstream
-        return "work";
-        $data = $req ->validated();
-        $image = $req->profileimage;
-        $imagename = time() . '.' . $image->extension();
-        $image->move(public_path('images'), $imagename);
-        $data['profileimage'] = $imagename;
-        Adminreg::create($data);
+  public function AdminRegSave(Request $req)
+  {
+    $image = $req->file('file');
+    $imagename = time() . '.' . $image->extension();
+    $image->move(public_path('images'), $imagename);
 
-        return back()->with("message", 'Student  Recored Has Been Created');
-=======
+    $data  = new Adminreg();
+    $data->firstname = $req->firstname;
+    $data->profileimage = $imagename;
 
+    $data->save();
 
-        $image = $req->file('file');
-        $imagename = time() . '.' . $image->extension();
-        $image->move(public_path('images'), $imagename);
-
-        $data  = new Adminreg();
-        $data->firstname = $req->firstname;
-        $data->profileimage = $imagename;
-
-        $data->save();
-
-        return back()->with("message", 'Your Request Has Been Pending');
-
-
-
->>>>>>> Stashed changes
-    }
+    return back()->with("message", 'Your Request Has Been Pending');
+  }
 }
